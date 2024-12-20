4098e957ad27bfd232186081281837d00359159c
KopikoCappu <MinhTran60524@gmail.com>
2024-12-19 20:17:47 -0500

    laptop recent push 12/19/20

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -11,6 +11,7 @@ import CustomFormField from "../ui/CustomFormField"
 import SubmitButton from "./SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
+import { useRouter } from "next/router";
  
 export enum FormFieldType {
   INPUT = "input",
@@ -24,6 +25,7 @@ export enum FormFieldType {
 
  
 const PatientForm = () => {
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
   const form = useForm<z.infer<typeof UserFormValidation>>({
     resolver: zodResolver(UserFormValidation),
@@ -34,9 +36,22 @@ const PatientForm = () => {
     },
   })
  
-  function onSubmit(values: z.infer<typeof UserFormValidation>) {
-    console.log(values)
+  async function onSubmit ({name, email, phone}: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+
+    try {
+      // const userData = {name, email, phone};
+
+      // const user = await createUser(userData);
+
+      // if(user) router.push(`/patients/${user.$id}/register`)
+    } catch (error) {
+      console.log(error);
+    }
+
   }
+
+
   return (
     <Form {...form}>
     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
