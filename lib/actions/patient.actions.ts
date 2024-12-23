c79d66b1b639ca0abc5ec70a8e17511841289b2e
Minh Tran <minhtran60524@gmail.com>
2024-12-23 00:04:21 -0500

    moving to laptop

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,3 +1,34 @@
-export const createuser = async (user: CreateUserParams) => {
-    
+"use server";
+
+import { ID, Query } from "node-appwrite";
+
+import {
+  BUCKET_ID,
+  DATABASE_ID,
+  ENDPOINT,
+  PATIENT_COLLECTION_ID,
+  PROJECT_ID,
+  databases,
+  storage,
+  users,
+} from "../appwrite.config";
+
+export const createUser = async (user: CreateUserParams) => {
+    try {
+        const newUser = await users.create(
+            ID.unique(),
+            user.email,
+            user.phone,
+            undefined,
+            user.name)
+        return newUser
+    } catch (error: any) {
+        if (error && error?.code === 409) {
+            const documents = await users.list([
+                Query.equal('email', [user.email])
+            ])
+
+            return documents?.users[0]
+        }
+    }
 }
