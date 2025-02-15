4220f7adf55fda2d29b1969a5e7d979894b989bf
Minh Tran <minhtran60524@gmail.com>
2025-02-15 08:58:26 -0500

    Almost there

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,15 +1,17 @@
-// This file configures the initialization of Sentry on the client.
-// The config you add here will be used whenever a users loads a page in their browser.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+// next.config.js
+const { withSentryConfig } = require("@sentry/nextjs");
 
-import * as Sentry from "@sentry/nextjs";
+const nextConfig = {
+  // Your Next.js config here
+};
 
-Sentry.init({
-  dsn: "https://56965d9bbb2315b125977d1018593d83@o4508822929342464.ingest.us.sentry.io/4508823013949440",
+const sentryWebpackPluginOptions = {
+  // Only in CI, upload source maps for Sentry
+  silent: !process.env.CI,
+  // Auto delete source maps after upload
+  sourcemaps: {
+    deleteSourcemapsAfterUpload: true,
+  },
+};
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
