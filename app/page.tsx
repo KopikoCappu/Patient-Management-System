12c99d0e537177c7b8edf99d32367f840ce7fb4a
Minh Tran <minhtran60524@gmail.com>
2025-02-18 04:15:28 -0500

    github advice

--- app/page.tsx
+++ app/page.tsx
@@ -3,46 +3,44 @@ import PasskeyModal from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Home({ searchParams} : SearchParamProps) {
+type SearchParamProps = {
+  searchParams: Promise<{ admin?: string }>;
+};
+
+export default async function Home({ searchParams }: SearchParamProps) {
   const resolvedSearchParams = await searchParams;
   const isAdmin = resolvedSearchParams.admin === 'true';
 
   return (
     <div className="flex h-screen max-h-screen">
-
-      {isAdmin && <PasskeyModal/>}
-
-
+      {isAdmin && <PasskeyModal />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image 
-            src = "assets\icons\logo-full.svg"
-            height = {1000}
-            width = {1000}
-            alt = "patient"
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
             className="mb-12 h-10 w-fit"
           />
-
-          <PatientForm/>
-
+          <PatientForm />
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-            © 2024 CarePulse
+              © 2024 CarePulse
             </p>
-            <Link href = "/?admin=true" className="text-green-500">
+            <Link href="/?admin=true" className="text-green-500">
               Admin
             </Link>
           </div>
         </div>
       </section>
-
       <Image
-        src = "/assets/images/onboarding-img.png"
-        height = {1000}
-        width = {1000}
-        alt = "patient"
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
         className="side-img max-w-[50%]"
       />
     </div>
-  )
+  );
 }
