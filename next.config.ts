e09cafce333ba2b8242f8cd526eda1fb86aedfca
Minh Tran <minhtran60524@gmail.com>
2025-02-18 02:32:33 -0500

    sentry removal

--- next.config.ts
+++ next.config.ts
@@ -1,43 +1,7 @@
-import { withSentryConfig } from "@sentry/nextjs";
-import type { NextConfig } from "next";
+ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withSentryConfig(nextConfig, {
-  // Sentry Project and Org configuration
-  org: "university-of-florida",
-  project: "javascript-nextjs",
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
-  reactComponentAnnotation: {
-    enabled: true,
-  },
-
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers
-  // tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors (does not yet work with App Router route handlers)
-  automaticVercelMonitors: true,
-
-  // Automatically delete source maps after they are uploaded to Sentry
-  sourcemaps: {
-    deleteSourcemapsAfterUpload: true,
-  },
-
-  // Disable telemetry data collection
-  telemetry: false,
-});
+ 
